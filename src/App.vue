8399c64581fea9c2dcaeded3f1fbedf9644727e1
Mufaddal Barwaniwala <invinciblemuffi@gmail.com>
2021-12-13 19:26:44 +0530

    Player Cards Coponent registered

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,25 @@
   <header>
     <h1>Monster Slayer</h1>
   </header>
+  <PlayerCards />
 </template>
 
 <script>
+import PlayerCards from "./components/PlayerCards.vue";
+export default {
+  components: PlayerCards,
+};
 </script>
 
 <style scoped>
-body {
-  width: 50%;
-  margin: 0 auto;
-}
-
 header {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 7em;
+  padding: 5em 10em;
   background: darkblue;
 }
+
 h1 {
   color: whitesmoke;
   font-size: 2.5rem;
