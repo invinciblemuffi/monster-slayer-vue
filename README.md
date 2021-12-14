02381e559ed8732e0e8923e9566d3f9b8b33c454
Mufaddal Barwaniwala <invinciblemuffi@gmail.com>
2021-12-14 11:57:26 +0530

    HTML-CSS code complete

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/b9eef9bd-17fd-447d-91b4-d43d94bb3a94/deploy-status)](https://app.netlify.com/sites/monster-slayer-vue3/deploys)
+
 # Monster Slayer Game with Vue 3
 
 - Deployed on ```https://ecstatic-pike-6ad744.netlify.app/```
-
-[![Netlify Status](https://api.netlify.com/api/v1/badges/b9eef9bd-17fd-447d-91b4-d43d94bb3a94/deploy-status)](https://app.netlify.com/sites/monster-slayer-vue3/deploys)
--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,31 @@
   <header>
     <h1>Monster Slayer</h1>
   </header>
-  <PlayerCards />
+  <section id="monster" class="container">
+    <h2>Monster Health</h2>
+    <div class="healthbar">
+      <div class="healthbar__value"></div>
+    </div>
+  </section>
+  <section id="monster" class="container">
+    <h2>Your Health</h2>
+    <div class="healthbar">
+      <div class="healthbar__value"></div>
+    </div>
+  </section>
+  <section id="controls">
+    <button>ATTACK</button>
+    <button>SPECIAL ATTACK</button>
+    <button>HEAL</button>
+    <button>SURRENDER</button>
+  </section>
+  <section id="log" class="container">
+    <h2>Battle Log</h2>
+    <ul></ul>
+  </section>
 </template>
 
 <script>
-import PlayerCards from "./components/PlayerCards.vue";
-export default {
-  components: PlayerCards,
-};
 </script>
 
 <style scoped>
@@ -17,12 +34,112 @@ header {
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 5em 10em;
-  background: darkblue;
+  background: darkred;
 }
 
 h1 {
   color: whitesmoke;
   font-size: 2.5rem;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2em;
+  border-radius: 0.75em;
+  background-color: whitesmoke;
+}
+
+section {
+  width: 85%;
+  max-width: 85%;
+  margin: 1em auto;
+}
+
+.healthbar {
+  width: 100%;
+  height: 40px;
+  border: 1px solid #575757;
+  margin: 1rem 0;
+  background: #fde5e5;
+}
+
+.healthbar__value {
+  background-color: #00a876;
+  width: 100%;
+  height: 100%;
+}
+
+#monster h2,
+#player h2 {
+  margin: 0.25rem;
+  width: 30%;
+}
+
+#controls {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #88005b;
+  background-color: #88005b;
+  color: white;
+  padding: 1rem 2rem;
+  border-radius: 12px;
+  margin: 1rem;
+  width: 12rem;
+  cursor: pointer;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+button:focus {
+  outline: none;
+}
+
+button:hover,
+button:active {
+  background-color: #af0a78;
+  border-color: #af0a78;
+  box-shadow: 1px 1px 8px rgba(0, 0, 0, 0.26);
+}
+
+button:disabled {
+  background-color: #ccc;
+  border-color: #ccc;
+  box-shadow: none;
+  color: #3f3f3f;
+  cursor: not-allowed;
+}
+
+#log ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+#log li {
+  margin: 0.5rem 0;
+}
+
+.log--player {
+  color: #7700ff;
+}
+
+.log--monster {
+  color: #da8d00;
+}
+
+.log--damage {
+  color: red;
+}
+
+.log--heal {
+  color: green;
 }
 </style>
